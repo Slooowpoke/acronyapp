c333801b860c00777d1b99614149daa573f1d962
Mali <malii.coxon@gmail.com>
2017-08-23 00:20:16 +0100

    Refactored NetworkProvider

--- app/helper/NetworkProvider.js
+++ app/helper/NetworkProvider.js
@@ -1,39 +1,41 @@
 import Config from 'react-native-config'
 
-class NetworkProvider{
-	// Add your own optional parameters to each method
-	// eg: in the case of needing authentication
+class NetworkProvider {
+    // Add your own optional parameters to each method
+    // eg: in the case of needing authentication
 
-	static view(endpoint){
-		console.log("Viewing");
-	}
+    static fetch(endpoint, id) {
+		return this.netRequest(Config.API_URL + '/' + endpoint + '/' + id,'GET', null);
+    }
 
-	static index(endpoint){
-		return fetch(Config.API_URL + '/' + endpoint, {
-            method: 'GET',
+    static search(endpoint) {
+		return this.netRequest(Config.API_URL + '/' + endpoint + '/search','GET', null);
+    }
+
+    static store(endpoint, data) {
+		return this.netRequest(Config.API_URL + '/' + endpoint + '/save','POST', data);
+    }
+
+	static netRequest(url, method, data){
+		// Generate the paramters
+		let parameters = {
+			method: method,
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
-            }
-        }).then((response) => response.json()).then((json) => {
+            },
+		}
+
+		if(data) parameters.body = JSON.stringify(data);
+
+		return fetch(url, {
+			parameters
+		}).then((response) => response.json()).then((json) => {
 			return json;
-        }).catch((error) => {
-            console.error(error);
-        });
+		}).catch((error) => {
+			console.error(error);
+		});
 	}
-
-	static create(endpoint){
-		console.log("Creating");
-	}
-
-	static update(endpoint){
-		console.log("Updating");
-	}
-
-	static delete(endpoint){
-		console.log("Deleting");
-	}
-
 }
 
 export default NetworkProvider;
