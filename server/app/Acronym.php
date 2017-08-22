17cdc0c359117dfd02e05e85d4cb54b8d353cc4d
Mali <malii.coxon@gmail.com>
2017-08-23 00:19:40 +0100

    Added relationship to acronym model

--- server/app/Acronym.php
+++ server/app/Acronym.php
@@ -5,6 +5,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Acronym extends Model
 {
- 	protected $fillable = ['acronym','meaning', 'description', 'popularity','industry'];
+ 	protected $fillable = ['acronym','meaning', 'description', 'popularity'];
+
+	public function industries()
+  	{
+	  return $this->hasMany('App\Industry', 'id');
+  	}
+
 }
 ?>
