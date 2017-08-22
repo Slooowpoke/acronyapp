c5aca3d214fd17b6c22ca65f6139cfc3137f0c2c
Mali <malii.coxon@gmail.com>
2017-08-23 00:19:31 +0100

    Added relationship to industry model

--- server/app/Industry.php
+++ server/app/Industry.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 class Industry extends Model
 {
  	protected $fillable = ['name'];
+
+	public function acronyms()
+	{
+		return $this->belongsToMany('App\Acronym')->orderBy('created_at','DESC')->withTimestamps();
+	}
 }
 ?>
