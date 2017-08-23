64b4ca0b2f78c815d1257fd27b3603172ec0925d
Mali <malii.coxon@gmail.com>
2017-08-24 00:05:58 +0100

    Added basic Suggestions shell

---
+++ app/components/Suggestions.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View, Button, TextInput} from 'react-native';
+
+import Config from 'react-native-config'
+import NP from '../helper/NetworkProvider'
+
+class Suggestions extends React.Component {
+
+    state = {data:[]};
+
+    componentWillMount() {
+		// Fetch the data
+		NP.search('acronym').then((response) => {
+
+			// Load the data for ListView
+			const ds = new ListView.DataSource({
+				rowHasChanged: (r1, r2) => r1 !== r2
+			});
+			this.setState({data:ds.cloneWithRows(response)});
+        });
+	}
+
+    render() {
+        return (
+            <View>
+                <Text>Suggestions</Text>
+				<Text>Horizontal ruling to go here.</Text>
+				{this.state.data != null
+					? (<ListView dataSource={this.state.data} renderRow={(item) => <Text>{item.acronym}</Text>}/>)
+					: <Text></Text>}
+            </View>
+        );
+    }
+}
+
+export default Suggestions;
