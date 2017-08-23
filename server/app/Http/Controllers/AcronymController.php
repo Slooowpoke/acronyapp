43b114e5a608f4f04c9566abdb04e209833ad1f5
Mali <malii.coxon@gmail.com>
2017-08-23 23:45:31 +0100

    Added method for saving Acronyms and Industries, with validation

--- server/app/Http/Controllers/AcronymController.php
+++ server/app/Http/Controllers/AcronymController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\Acronym;
+use App\Industry;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AcronymController extends Controller{
 
@@ -17,22 +20,82 @@ class AcronymController extends Controller{
     {
     }
 
-	public function store(Request $request){
+	public function search(Request $request, $terms){
+		$acronyms  = Acronym::all();
 
-    	$acronym = Acronym::create($request->all());
-
-    	return response()->json($acronym);
+    	return response()->json($acronyms);
 	}
 
-	public function update(Request $request, $id){
+	public function store(Request $request){
 
-    	$acronym  = Acronym::find($id);
-    	$acronym->acronym = $request->input('acronym');
-    	$acronym->description = $request->input('description');
-    	$acronym->popularity = $request->input('popularity');
-    	$acronym->save();
+		// Validate the acronym before saving it
+	// 	$this->validate($request, [
+	// 	   'acronym' => 'required|string|max:255',
+	// 	   'meaning' => 'required|string|max:255|uniquewith',
+	// 	   'description' => 'required|string|max:512',
+	// 	   'industry[*]name' => 'required|string|max:255',
+	//    ]);
 
-    	return response()->json($acronym);
+		$validator = Validator::make($request->all(), [
+		   'acronym' => 'required|string|max:255',
+		   'meaning' => 'required|string|max:255',
+		   'description' => 'required|string|max:512',
+		   'industry[*]name' => 'required|string|max:255',
+		]);
+
+		$meaning = $request->input('meaning');
+		$description = $request->input('description');
+
+		// Check to see if the same acronym and meaning exist
+		$results = app('db')->select("SELECT * FROM `acronyms` WHERE `meaning` = '$meaning' AND `description` = '$description'");
+
+		if(count($results) >= 1){
+	    	$validator->errors()->add('Acronym', "Already exists");
+		}
+
+		if(count($validator->errors()) > 0) {
+			return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+		// Create the acronym
+		$acronym = Acronym::create([
+            'acronym' => $request->input('acronym'),
+            'meaning' => $request->input('meaning'),
+            'description' => $request->input('description'),
+            'popularity' => 0,
+			'safe' => 0,
+        ]);
+
+
+		$industries = $request->input('industries');
+
+		$industryModels = collect([]);
+
+		foreach($industries as $industryName){
+
+			// Check each of these industries to see if the exist in our database already.
+			$result = Industry::where('name',$industryName)->first();
+
+			if(!$result){
+				// Create the industry
+				$industryModel = Industry::create(['name' => $industryName]);
+			}else{
+				// If we do find a result, set our model to be it
+				$industryModel = $result;
+			}
+
+			// Now sync the Acronym to the industies
+			$industryModel->acronyms()->sync([$acronym->id], false);
+
+			$industryModels->push($industryModel);
+		}
+		die();
+    	return response()->json(['acronym' => $acronym,'industries' => $industryModels]);
+	}
+
+	public function fetch(Request $request,$id){
+		$acronym  = Acronym::find($id);
+		return response()->json($acronym);
 	}
 
 	public function destroy($id){
@@ -40,16 +103,5 @@ class AcronymController extends Controller{
     	$acronym->delete();
 
     	return response()->json('Deleted.');
-	}
-
-	public function index(){
-    	$acronyms  = Acronym::all();
-
-    	return response()->json($acronyms);
-	}
-
-	public function fetch(Request $request,$id){
-		$acronym  = Acronym::find($id);
-		return response()->json($acronym);
 	}
 }
