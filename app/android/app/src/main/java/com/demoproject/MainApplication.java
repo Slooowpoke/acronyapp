cd6da5445a64fd8d484b129d44eb81047a095d71
Mali <malii.coxon@gmail.com>
2017-08-29 22:09:12 +0100

    Removed dead dependencies

--- app/android/app/src/main/java/com/demoproject/MainApplication.java
+++ app/android/app/src/main/java/com/demoproject/MainApplication.java
@@ -3,8 +3,6 @@ package com.demoproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,8 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new LinearGradientPackage(),
-            new RNSpinkitPackage(),
             new VectorIconsPackage(),
             new ReactNativeConfigPackage()
       );
