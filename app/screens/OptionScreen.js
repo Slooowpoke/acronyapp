c05e542b8fee71e035bcab3a2e90a41513624a14
Mali <malii.coxon@gmail.com>
2017-08-29 22:29:02 +0100

    Added options screen with styles

--- app/screens/OptionScreen.js
+++ app/screens/OptionScreen.js
@@ -1,30 +1,122 @@
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View, Button} from 'react-native';
+import {AppRegistry, StyleSheet, Text, View, Button, Dimensions, Switch} from 'react-native';
 
 import Config from 'react-native-config'
 
-class ChatScreen extends React.Component {
+import Footer from '../components/Footer'
+
+import TagInput from 'react-native-tag-input';
+global.dimensions = {width, height} = Dimensions.get('window');
+
+class OptionScreen extends React.Component {
 	static navigationOptions = {
-		title: 'Options'
+		headerTitle: <Text style={{marginLeft:20}}>Dashboard</Text>
 	};
 
 	state = {
-		counter:0,
+		suitableForWork: false,
+		industries: [],
 	};
 
 	componentWillMount(){
+		console.log(global.dimensions);
+		if(global.industries == null){
+			global.industries = [];
+			global.industries.push('General');
+		}
+		this.setState({industries: global.industries});
 
 	}
 
     render() {
-        const {params} = this.props.navigation.state;
         return (
-            <View>
+            <View style={[styles.container, {minHeight:global.dimensions.height}]}>
 
+				<Text style={styles.header}>Options here will affect the creation and browsing of acronyms.</Text>
 
+				<View style={styles.industrySelector}>
+					<TagInput
+						tagTextStyle={{fontFamily: 'Ubuntu'}}
+					  value={this.state.industries}
+					  onChange={(industries) => this.onIndustryChange(industries)} placeholder="Type industry here."/>
+				</View>
+
+				<View style={styles.suitable}>
+
+					<View style={styles.label}>
+						<Text style={{fontFamily: 'Ubuntu'}}>{this.state.suitableForWork ? "Showing: Suitable for work. (SFW)" : "Showing: Not suitable for work. (NSFW)"}</Text>
+					</View>
+
+					<View style={styles.switch}>
+						<Switch onValueChange={this.toggleSwitch} value={this.state.suitableForWork}/>
+					</View>
+				</View>
+
+				<Button style={styles.save} title="Save changes" onPress={() => this.saveOptions()} />
+
+				<Footer/>
             </View>
         );
     }
+
+	onIndustryChange(industries){
+		this.setState({industries: industries});
+	}
+
+ 	toggleSwitch = (value) => {
+		this.setState({ suitableForWork: value })
+		// We also need to set the glob\al too
+		global.suitableForWork = value;
+	}
+
+	saveOptions(){
+		// Save the options state to either json or some form of persistent storage?
+		// TODO decide SQLLite or just regular json
+		global.industries = this.state.industries;
+		console.log(global.industries);
+	}
+
 }
 
-export default ChatScreen;
+const styles = StyleSheet.create({
+    container: {
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: '#fff',
+        flex: 1,
+        flexGrow: 1,
+		justifyContent:'space-around'
+    },
+	header:{
+		fontSize: 18,
+		marginTop:30,
+		textAlign:'center',
+		fontFamily: 'Ubuntu'
+	},
+	industrySelector:{
+		borderWidth:1,
+		borderColor:'#ddd',
+		height:140,
+		padding:16,
+		marginTop:20,
+		marginBottom:10,
+	},
+	suitable:{
+		flexDirection:'row',
+	},
+	label:{
+		marginTop:10,
+		alignItems:'flex-start',
+		width: (global.dimensions.width/2)+80,
+	},
+	switch:{
+		marginTop:5,
+		width: (global.dimensions.width/2)-120,
+		alignItems:'flex-end',
+	},
+	save:{
+		marginTop:30,
+	}
+});
+
+export default OptionScreen;
