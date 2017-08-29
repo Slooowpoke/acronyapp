c239c24901213eebaf892c18773587a952595a57
Mali <malii.coxon@gmail.com>
2017-08-29 22:07:28 +0100

    Added class to App and extra comments

--- app/App.js
+++ app/App.js
@@ -12,7 +12,11 @@ import {
 
 import Config from 'react-native-config'
 
+// Import our navigation
+
 import {StackNavigator, TabNavigator} from 'react-navigation';
+
+// Import our screens
 
 import BrowseScreen from './screens/BrowseScreen';
 import OptionScreen from './screens/OptionScreen';
@@ -39,4 +43,48 @@ const ApplicationNavigator = StackNavigator({
     }
 });
 
-AppRegistry.registerComponent('DemoProject', () => ApplicationNavigator);
+class App extends React.Component {
+	componentWillMount(){
+		global.alerts = [];
+		global.alerts.push({message: "Hello, app has begun.", visibility:true});
+		this.setState({alerts: global.alerts});
+	}
+
+    someEvent() {
+        // call navigate for AppNavigator here:
+        this.navigator && this.navigator.dispatch({type: 'Navigate', routeName, params});
+    }
+
+	constructor(props) {
+        super(props)
+
+        this.state = {
+            alerts:global.alerts,
+        }
+    }
+    render() {
+        return (
+            <View style={{flex:1}}>
+                <StatusBarAlert
+					visible={global.alerts[0].visibility}
+					message={global.alerts[0].message}
+					backgroundColor="#3CC29E"
+					color="white"
+					onPress={() => this.closeAlert()}/>
+                <ApplicationNavigator ref={nav => {
+                    this.navigator = nav;
+                }}/>
+            </View>
+
+        );
+    }
+
+	closeAlert(){
+		let temp = this.state.alerts;
+		temp[0].visibility = false;
+		this.setState({alerts: temp});
+		global.alerts = temp;
+	}
+}
+
+AppRegistry.registerComponent('DemoProject', () => App);
